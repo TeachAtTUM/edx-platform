f10b85bdc4972c82796d598735857805190f6da8
Eric Fischer <efischer@edx.org>
2016-06-13 15:51:37 -0400

    Celery routing (#12727)
    * Use routing

--- openedx/core/djangoapps/content/block_structure/tasks.py
+++ openedx/core/djangoapps/content/block_structure/tasks.py
@@ -3,7 +3,6 @@ Asynchronous tasks related to the Course Blocks sub-application.
 """
 import logging
 from celery.task import task
-from django.conf import settings
 from opaque_keys.edx.keys import CourseKey
 
 from . import api
@@ -11,19 +10,7 @@ from . import api
 log = logging.getLogger('edx.celery.task')
 
 
-def _ensure_lms_queue():
-    """
-    Ensure the worker associated with the chosen queue is loaded with lms params, if available.
-
-    This is needed due to the signal that triggers these tasks coming from cms, which does not have settings
-    defined that are required for these tasks.
-    """
-    queues = getattr(settings, 'CELERY_QUEUES', None)
-    lms_queue = next((queue for queue in queues if '.lms.' in queue), None)
-    return lms_queue
-
-
-@task(queue=_ensure_lms_queue())
+@task
 def update_course_in_cache(course_key):
     """
     Updates the course blocks (in the database) for the specified course.
@@ -32,7 +19,7 @@ def update_course_in_cache(course_key):
     api.update_course_in_cache(course_key)
 
 
-@task(queue=_ensure_lms_queue())
+@task
 def clear_course_from_cache(course_key):
     """
     Deletes the given course from the cache.
